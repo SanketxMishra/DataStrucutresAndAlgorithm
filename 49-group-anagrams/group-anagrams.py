c458cbde32b66c2257c037f24c4dbc325323f35c
Sanket Mishra <101252236+SanketxMishra@users.noreply.github.com>
2025-09-14 04:17:20 +0530

    Time: 29 ms (34.86%) | Memory: 16 MB (93.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def groupAnagrams(self, strs):
         out = defaultdict(list)
-        for word in strs:
-            # count frequency of each character
-            count = [0] * 26  
-            for ch in word:
-                count[ord(ch) - ord('a')] += 1
-            # use tuple as key (hashable)
-            out[tuple(count)].append(word)
+        for i in strs :
+            x = ''.join(sorted(i))
+            out[x].append(i)
         return list(out.values())
+        
