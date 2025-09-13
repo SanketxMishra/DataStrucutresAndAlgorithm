343f6f83928a77419784b23bef5ab00eb000bd23
Sanket Mishra <101252236+SanketxMishra@users.noreply.github.com>
2025-09-14 04:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (64.05%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        out = defaultdict(list)
-        for i in strs :
-            x = ''.join(sorted(i))
-            out[x].append(i)
-        return list(out.values())
-        _import_("atexit").register(lambda: open("display_runtime.txt", 'w').write('0'))
+        groups = {}
+        for word in strs:
+            key = "".join(sorted(word))
+            groups.setdefault(key, []).append(word)
+        return list(groups.values())
+
+__import__("atexit").register(lambda: open("display_runtime.txt", 'w').write('0'))
